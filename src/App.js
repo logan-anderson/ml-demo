aadca0eb738ef4197ee008533ad3dd045b9c24b8
Logan Anderson <landerson2@upei.ca>
2021-05-06 10:17:26 -0300

    it works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { data } from "./data";
+import { MathComponent } from "mathjax-react";
+
+const predictaion = (x0, x1, housePrice) => {
+  return x0 + x1 * housePrice;
+};
 
 function App() {
+  const [vars, setVars] = useState({
+    x0: 0.001,
+    x1: 0.001,
+  });
+  const [error, setError] = useState(0);
+  const deps = JSON.stringify(vars);
+  useEffect(() => {
+    let total = 0;
+    data.forEach((dataPt) => {
+      total =
+        total +
+        Math.pow(
+          dataPt.SalePrice - predictaion(vars.x0, vars.x1, dataPt.LotArea),
+          2
+        );
+    });
+    const err = Math.sqrt(total / data.length);
+    setError(err);
+  }, [deps]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>
+          x0
+          <input
+            onChange={(e) => {
+              e.preventDefault();
+              setVars({
+                ...vars,
+                x0: Number(e.target.value),
+              });
+            }}
+            name="x0"
+            type="number"
+            step={0.001}
+            value={vars.x0}
+          ></input>
+        </label>
+        <label>
+          x1
+          <input
+            onChange={(e) => {
+              e.preventDefault();
+              setVars({
+                ...vars,
+                x1: Number(e.target.value),
+              });
+            }}
+            name="x1"
+            type="number"
+            step={0.001}
+            value={vars.x1}
+          ></input>
+        </label>
+      </form>
+      <div>x0: {vars.x0}</div>
+      <div>x1: {vars.x1}</div>
+      <div>
+        Hypothesis functions:
+        <div>
+          <MathComponent
+            tex={`h(price) = ${vars.x0} + ${vars.x1} \\cdot price`}
+          />
+        </div>
+      </div>
+      <div>Error: {error}</div>
     </div>
   );
 }
